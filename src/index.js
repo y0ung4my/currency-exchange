aa345fce226b5c24a835909e7b354633662656ed
Amy Young <youngamy1223@gmail.com>
2022-01-15 00:28:01 -0700

    add USD converter using API call

--- src/index.js
+++ src/index.js
@@ -2,8 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import SampleClass from './sample-service.js'
+import CurrencyExchange from './sample-service.js'
+
+function getElements(response, startAmount) {
+  if (response) {
+    $('.showResult').text(`${startAmount} USD exchanges to ${(response.conversion_rate * startAmount).toFixed(2)} in ${response.target_code}`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+async function makeApiCall(startAmount, targetCurrency) {
+  const response = await CurrencyExchange.getExchangeRate(targetCurrency);
+  console.log(response);
+  getElements(response, startAmount);
+}
 
 $(document).ready(function() {
-SampleClass.getSampleApiCall();
+  $('#submit-button').click(function() {
+    const startAmount = parseFloat($('#start-amount').val());
+    const targetCurrency = $('#end-currency').val();
+    makeApiCall(startAmount, targetCurrency);
+  });
 });
--- src/sample-service.js
+++ src/sample-service.js
@@ -1,6 +1,13 @@
-export default class SampleService {  
-  static async getSampleApiCall() {
-    const nothing = null;
-    return nothing;
+export default class CurrencyExchange {  
+  static async getExchangeRate(targetCurrency) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/$${process.env.API_KEY}/pair/USD/${targetCurrency}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
